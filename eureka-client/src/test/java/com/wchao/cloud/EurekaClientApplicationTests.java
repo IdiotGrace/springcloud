bd74776391993d84629308ade2f2ddb15eafa057
haowenchao <haowenchao@haowenchaos-MacBook-Pro.local>
2020-12-28 20:24:15 +0800

    eureka

---
+++ eureka-client/src/test/java/com/wchao/cloud/EurekaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wchao.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
